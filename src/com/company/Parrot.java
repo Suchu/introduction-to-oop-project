25f6cd7b796228b194c8052d1923c645fd518643
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-09-13 02:57:22 +0100

    Implemented Parrot subclass

---
+++ src/com/company/Parrot.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Parrot extends Bird {
+
+    public Parrot(String name, String color) {
+        super(name, color);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Yo! I'm " + name + " and I'm a " + color + " Parrot. I can sing Oppa Gangnam Style!");
+    }
+
+    public static void main(String[] args) {
+        Parrot coco = new Parrot("Coco", "green");
+        coco.speak();
+        coco.fly();
+    }
+}
