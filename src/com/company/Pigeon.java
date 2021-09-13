0f6ed89c5240d4087d9df30f03913360b7ab5a74
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-09-13 02:10:31 +0100

    Implemented Exercise3 - Inheritance

---
+++ src/com/company/Pigeon.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Pigeon extends Bird{
+    public Pigeon(String name, String color) {
+        super(name, color);
+    }
+    public void eatPizza() {
+        System.out.println("Delicious pizza!");
+    }
+
+    public static void main(String[] args) {
+        Pigeon pippa = new Pigeon("Pippa", "Grey");
+        pippa.speak();
+        pippa.fly();
+        pippa.eatPizza();
+    }
+}
